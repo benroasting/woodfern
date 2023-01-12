d327b33174e3ee9cacaad074b8e547ebcde3cde6
Ben Young <ben.young@ramseysolutions.com>
2023-01-12 15:53:16 -0600

    starting work on woodfern website

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Woodfern Floral Design</title>
+        <meta name="description" content="Nashville Wedding Flowers" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/wood+fern+sticker+purpler+mauve.png" />
+      </Head>
+      <main>
+        <h1 className="text-3xl font-light text-yellow-600">Woodfern</h1>
+      </main>
+    </>
+  );
+}
